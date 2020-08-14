bf40797b5fe9d65842aec4f91e2fef60d6b41ce8
stellapath <stellapath@gmail.com>
2020-08-14 16:59:58 +0900

    메인 틀 업데이트

--- Project/app/src/main/java/com/hanul/project/MainActivity.java
+++ Project/app/src/main/java/com/hanul/project/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -59,7 +60,11 @@ public class MainActivity extends AppCompatActivity {
                         R.string.navigation_draw_open, R.string.navigation_draw_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
     }
+    /***********************************************************************************************
+     * onCreate();
+     ***********************************************************************************************/
 
     /**
      * 상단바 옵션
@@ -79,6 +84,19 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    /**
+     * 뒤로가기 버튼 눌렀을 때 드로어가 열려있으면 닫기
+     */
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 }
